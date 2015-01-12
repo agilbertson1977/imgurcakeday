5b0447737e457210779c614c003f68ee1484c5db
Andrew Gilbertson <agilbertson@gmail.com>
2015-01-12 15:32:58 -0500

    Just needs some tweaks to look cool! Maybe talk to Bethie?

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var http = require("http");
 var https = require("https");
 var fs = require('fs');
@@ -5,6 +7,7 @@ var fs = require('fs');
 var config = JSON.parse(fs.readFileSync('config.json'));
 
 var homepage = fs.readFileSync('main_page.html');
+var respage = new String(fs.readFileSync('response_page.html'));
 
 var httpsOptions = {
   hostname: "api.imgur.com",
@@ -21,6 +24,7 @@ function processRequest(CakeDayRequest, CakeDayResponse) {
   } else if (CakeDayRequest.url.indexOf("getcakeday?account") != -1) {
     console.log(CakeDayRequest.url.substring(20));
     var account = CakeDayRequest.url.substring(20);
+    account = account.replace("\+", "");
     var ApiResData = "";
     httpsOptions.path = "/3/account/" + account;
     var ApiReq = https.request(httpsOptions, function(ApiRes) {
@@ -29,12 +33,31 @@ function processRequest(CakeDayRequest, CakeDayResponse) {
         ApiResData = ApiResData + d; 
       });
       ApiRes.on('end', function() { 
-        ApiResJSON = JSON.parse(ApiResData);
-        CakeDayResponse.writeHead(200, {"Content-Type": "text/plain"});
+        var ApiResJSON = JSON.parse(ApiResData);
+        CakeDayResponse.writeHead(200, {"Content-Type": "text/html"});
         //CakeDayResponse.write(JSON.stringify(ApiResJSON, null, 2));
-        CakeDayResponse.write("\n\n\n\n");
         var d = new Date(ApiResJSON.data.created * 1000);
-        CakeDayResponse.write(d.toString());
+        var dd = "";
+        switch (d.getMonth()) {
+          case  0: dd+="Jan "; break;
+          case  1: dd+="Feb "; break;
+          case  2: dd+="Mar "; break;
+          case  3: dd+="Apr "; break;
+          case  4: dd+="May "; break;
+          case  5: dd+="Jun "; break;
+          case  6: dd+="Jul "; break;
+          case  7: dd+="Aug "; break;
+          case  8: dd+="Sep "; break;
+          case  9: dd+="Oct "; break;
+          case 10: dd+="Nov "; break;
+          case 11: dd+="Dec "; break;
+        }
+        dd += d.getDate() + ", ";
+        dd += d.getFullYear();
+        var intermediateResponse = respage.replace(":account:", account);
+        intermediateResponse = intermediateResponse.replace(":account:", account);
+        var thisResponse = intermediateResponse.replace(":cakeday:", dd);
+        CakeDayResponse.write(thisResponse);
         CakeDayResponse.end();
         console.log("response for " + account + " sent");
       });

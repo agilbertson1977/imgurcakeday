f72a7cf6ab780ef25d784bc48aff990e3d088d6d
Andrew Gilbertson <agilbertson@gmail.com>
2015-01-12 14:30:47 -0500

    Basics working, needs HTML template for the response page.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # imgurcakeday
+
+Just because imgur.com's account view only shows month and year, not day.  So I decided to fix it with a little Node.js magic.
--- main.js
+++ main.js
@@ -1,24 +1,57 @@
 var http = require("http");
+var https = require("https");
 var fs = require('fs');
 
 var config = JSON.parse(fs.readFileSync('config.json'));
 
 var homepage = fs.readFileSync('main_page.html');
 
-function processRequest(request, response) {
-  if (request.url === "/") {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(homepage);
-  } else if (request.url.indexOf("account") != -1) {
-    console.log(request.url);
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(homepage);
+var httpsOptions = {
+  hostname: "api.imgur.com",
+  port: 443,
+  method: "GET",
+  headers: { "Authorization": "Client-ID " + config.client_id }
+};
+
+function processRequest(CakeDayRequest, CakeDayResponse) {
+  if (CakeDayRequest.url === "/") {
+    CakeDayResponse.writeHead(200, {"Content-Type": "text/html"});
+    CakeDayResponse.write(homepage);
+    CakeDayResponse.end();
+  } else if (CakeDayRequest.url.indexOf("getcakeday?account") != -1) {
+    console.log(CakeDayRequest.url.substring(20));
+    var account = CakeDayRequest.url.substring(20);
+    var ApiResData = "";
+    httpsOptions.path = "/3/account/" + account;
+    var ApiReq = https.request(httpsOptions, function(ApiRes) {
+      ApiRes.on('data', function(d) { 
+        //console.log("Data received: " + d);
+        ApiResData = ApiResData + d; 
+      });
+      ApiRes.on('end', function() { 
+        ApiResJSON = JSON.parse(ApiResData);
+        CakeDayResponse.writeHead(200, {"Content-Type": "text/plain"});
+        //CakeDayResponse.write(JSON.stringify(ApiResJSON, null, 2));
+        CakeDayResponse.write("\n\n\n\n");
+        var d = new Date(ApiResJSON.data.created * 1000);
+        CakeDayResponse.write(d.toString());
+        CakeDayResponse.end();
+        console.log("response for " + account + " sent");
+      });
+    });
+    ApiReq.end();
+    ApiReq.on('error', function(e) {
+      console.log("Error: " + e);
+      CakeDayResponse.writeHead(500, {"Content-Type": "text/plain"});
+      CakeDayResponse.write("500 External Server Error");
+      CakeDayResponse.end();
+    });
   } else {
-    console.log("URL = \"" + request.url + "\"");
-    response.writeHead(404, {"Content-Type": "text/plain"});
-    response.write("Page Not Found");
+    console.log("URL = \"" + CakeDayRequest.url + "\"");
+    CakeDayResponse.writeHead(404, {"Content-Type": "text/plain"});
+    CakeDayResponse.write("Page Not Found");
+    CakeDayResponse.end();
   }
-  response.end();
 }
 
 
